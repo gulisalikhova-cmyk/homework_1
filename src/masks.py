80db11b6e4770c3de5f39aa8cafbcfb061fc9de2
Гулия Салихова <gulisalikhova@gmail.com>
2026-04-29 16:01:10 +0300

    init project

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция получает номер карты пользователя и возвращает замаскированный номер"""
+
+    card_number = card_number.replace(" ", "")
+
+    if len(card_number) != 16 or not card_number.isdigit():
+        raise ValueError("Номер карты должен содержать 20 цифр")
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+bank_card_number = input()
+print(get_mask_card_number(bank_card_number))
+
+
+def get_mask_account(account: str) -> str:
+    """Функция принимает номер счета и выводит его маску"""
+
+    account = account.replace(" ", "")
+
+    if len(account) != 20 or not account.isdigit():
+        raise ValueError("Номер счета должен содержать 16 цифр")
+    return f"**{account[-4:]}"
+
+
+user_account = input()
+print(get_mask_account(user_account))
