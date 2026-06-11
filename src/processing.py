0907fcd57016173b7fc435b3f6427c2f893669b7
Гулия Салихова <gulisalikhova@gmail.com>
2026-06-11 13:27:05 +0300

    first commit

---
+++ src/processing.py
@@ -0,0 +1,4 @@
+transactions = [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+ {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+def filter_by_state(list_of_dicts, state="EXECUTED"):
+    pass
