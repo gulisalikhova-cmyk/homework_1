140a008c1eb26e3e18b8a861912a05206b4f5b6f
Гулия Салихова <gulisalikhova@gmail.com>
2026-06-11 13:40:39 +0300

    create feature branch

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
+from typing import List, Dict, Any
+
 transactions = [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-def filter_by_state(list_of_dicts, state="EXECUTED"):
+def filter_by_state(list_of_dicts: List[Dict[str, Any]], state="EXECUTED"):
     pass
