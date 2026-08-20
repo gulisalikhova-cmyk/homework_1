f7a6aa516eae16b0545c036e6609224675ea1e1a
Гулия Салихова <gulisalikhova@gmail.com>
2026-08-20 21:05:12 +0300

    added card number generator

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,11 @@ def transaction_descriptions(transactions):
     """Возвращает описания транзакций."""
     for transaction in transactions:
         yield transaction.get("description", "Описание отсутствует")
+
+
+def card_number_generator(start, end):
+    """Генерирует номера карт в формате XXXX XXXX XXXX XXXX."""
+    for number in range(start, end + 1):
+        card_str = str(number).zfill(16)
+        formatted = " ".join([card_str[i:i+4] for i in range(0, 16, 4)])
+        yield formatted
