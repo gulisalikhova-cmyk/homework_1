8f02e716442f4c337f480a9a072cc647343344cf
Гулия Салихова <gulisalikhova@gmail.com>
2026-08-20 21:01:55 +0300

    added transaction descriptions

--- src/generators.py
+++ src/generators.py
@@ -4,3 +4,9 @@ def filter_by_currency(transactions, currency_code):
         if "operationAmount" in transaction and "currency" in transaction["operationAmount"]:
             if transaction["operationAmount"]["currency"].get("code") == currency_code:
                 yield transaction
+
+
+def transaction_descriptions(transactions):
+    """Возвращает описания транзакций."""
+    for transaction in transactions:
+        yield transaction.get("description", "Описание отсутствует")
